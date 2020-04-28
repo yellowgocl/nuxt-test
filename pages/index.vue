ad559498fa471b8b0a436db200db187a73a9cc1b
got.wong <wangzhaojian@zhibankeji.com>
2020-04-28 13:49:41 +0800

    -

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bg-primary pb-4">
+      <navbar />
+      <div class="container mx-auto">
+        <div class="px-2 lg:px-96 mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+            <card class='' v-for="(item) in cards" :key='item.name' :data='item'></card>
+        </div>
+        <div class="pad py-8"></div>
+        <div class="action-main w-2/3 mx-auto lg mt-8">Back</div>
+      </div>
+      <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+import Card from '~/components/Card.vue'
+import Footer from '~/components/Footer.vue'
+export default {
+  components: {
+    Navbar, Card, 'app-footer': Footer
+  },
+  data() {
+    return {
+      cards: [
+        { image: 'https://placem.at/places?h=400&random=1&txt', name: 'Category Seed1' },
+        { image: 'https://placem.at/places?h=400&random=2&txt', name: 'Category Seed2' },
+        
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  
+}
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#050d0f',
+        app: {
+          default: '#ddca88',
+          "1": '#88754b'
+        },
+        'divider-primary': '#654423'
+      },
+      spacing: {
+        '96': '6rem',
+      },
+      height: {
+        'navbar': '64px',
+        'navbar-md': '96px',
+        'navbar-lg': '144px',
+        'navbar-xl': '188px',
+        'footer': '64px',
+        'footer-md': '96px',
+        'footer-lg': '144px',
+        'footer-xl': '188px'
+      }
+    },
+  },
+  fontFamily: {
+    display: ['Roboto', 'sans-serif'],
+    body: ['Roboto', 'sans-serif'],
+  },
+  variants: {},
+  plugins: [],
+}
