36227f364a18a56f8a3008698629d54e572a8965
got.wong <wangzhaojian@zhibankeji.com>
2020-04-28 13:34:06 +0800

    no message

--- components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mt-8 footer flex justify-center items-center h-footer md:h-footer-md lg:h-footer-lg xl:h-footer-xl">
-        <span class=' text-app text-center text-md'>im footer</span>
-    </div>
-</template>
